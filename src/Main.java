6dc8ec546eb96d6e381e4cb38bc0d142c303cf3e
Adriana Andrés Visquert <adriana.anvi@gmail.com>
2025-12-04 19:49:07 +0100

    Cambio realizado desde el terminal

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("¡Hola desde IntelliJ y Git! ");
+        System.out.println("¡Hola desde el terminal y la NuevaRama!");
     }
 }
